69a02aec2c123560ed714f927c727b61437d9df3
AfzalHassan <afzal.hassan@arimaclanka.com>
2025-10-20 06:32:26 +0530

    Added more localization keys and values

--- lib/app.dart
+++ lib/app.dart
@@ -9,7 +9,7 @@ class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp.router(
-      debugShowCheckedModeBanner: false,
+      key: ValueKey(context.locale.toString()),
       locale: context.locale,
       localizationsDelegates: context.localizationDelegates,
       routerConfig: AppRouter.router,
