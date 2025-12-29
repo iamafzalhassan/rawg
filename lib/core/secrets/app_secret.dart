2d9c29819b1b6954e900aa8732f52328b7d66bf4
AfzalHassan <afzal.hassan@arimaclanka.com>
2025-12-29 14:21:11 +0530

    Added OneSignal push notification

--- lib/core/secrets/app_secret.dart
+++ lib/core/secrets/app_secret.dart
@@ -1,6 +1,6 @@
 class AppSecret {
-  static const rawgApiKey = 'b7faad886815457bacd67c77eea31f81';
-  static const supaBaseUrl = 'https://ukuvjfpdadbsvcrsnjem.supabase.co';
-  static const supaBaseApiKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InVrdXZqZnBkYWRic3ZjcnNuamVtIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjE3OTk2NTMsImV4cCI6MjA3NzM3NTY1M30.toCHdP6GgBNsh5-0Y2BogfmTBmHVQ2kpz-EHBMlHzKQ';
+  static const rawgApiKey = '';
+  static const supaBaseUrl = '';
+  static const supaBaseApiKey = '';
   static const oneSignalAppId = '';
 }
